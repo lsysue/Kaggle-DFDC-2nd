72e94d8b2eb94b3aba5ac4de646b39847ac6fb19
suyuan <lsyxll268@163.com>
2020-11-20 09:04:57 +0800

    update evaluation

---
+++ cross_entropy.py
@@ -0,0 +1,36 @@
+import math
+from collections import defaultdict
+
+def cross_entropy(gtruth_csv, result_csv):
+    with open(gtruth_csv) as gf, open(result_csv) as rf:
+        next(gf)
+        next(rf)
+        num = 0  # 计数预测的个数
+        cross_entropy_loss = 0.0
+        gtruth = defaultdict(lambda:0.0)
+        result = defaultdict(lambda:0.0)
+        # 处理result_csv
+        while True:
+            rline = rf.readline()
+            if not rline:
+                break
+            fname = rline.split("\t")[0]
+            if 0 == int(rline.split("\t")[2]):
+                continue
+            result[fname] = float(rline.split("\t")[1]) # 读取预测结果
+            num = num + 1
+
+        while True:
+            gline = gf.readline()
+            if not gline:
+                break
+            fname = gline.split(",")[0]
+            if fname in result.keys():
+                gtruth[fname] = float(gline.split(",")[1]) # 读取实际结果
+                cross_entropy_loss = cross_entropy_loss - (gtruth[fname] * math.log2(result[fname]) + (1 - gtruth[fname]) * math.log2(1 - result[fname]))
+                # print("info | cross_entropy_loss:", cross_entropy_loss, - (gtruth[fname] * math.log2(result[fname]) + (1 - gtruth[fname]) * math.log2(1 - result[fname])))
+        cross_entropy_loss = cross_entropy_loss / num
+    return cross_entropy_loss, num
+
+loss, video_num = cross_entropy("./dfdc-data.csv", "./submission.csv")
+print("RESULT | cross entropy loss: %.8f | video num: %d" % (loss, video_num))
--- submission.py
+++ submission.py
@@ -29,6 +29,9 @@ class DFDCLoader:
 
         self.record = defaultdict(list)
         self.score = defaultdict(lambda: 0.5)
+        # 推断时间
+        self.infer_start = defaultdict(lambda: 0)
+        self.infer_end = defaultdict(lambda: 0)
         self.feedback_queue = []
 
     def iter_one_face(self):
@@ -82,6 +85,8 @@ class DFDCLoader:
 
         for fname, face in self.iter_one_face():
             self.feedback_queue.append(fname)
+            if not batch_buf:   # batch_buf is clear
+                self.infer_start[fname] = time.time()
             batch_buf.append(face)
 
             if len(batch_buf) == self.batch_size:
@@ -89,10 +94,11 @@ class DFDCLoader:
 
                 batch_count += 1
                 batch_buf.clear()
-
                 if batch_count % 10 == 0:
                     elapsed = 1000 * (time.time() - t0)
                     print("T: %.2f ms / batch" % (elapsed / batch_count))
+                    # 测试控制
+                    # break
 
         if len(batch_buf) > 0:
             yield torch.stack(batch_buf)
@@ -107,33 +113,41 @@ class DFDCLoader:
 
         for fname in sorted(accessed):
             self.score[fname] = np.mean(self.record[fname])
-            print("[%s] %.6f" % (fname, self.score[fname]))
+            # 推断开始时间，推断结束时间
+            self.infer_end[fname] = time.time()
+            print("[%s] %.6f | %.3f ms" % (fname, self.score[fname], (self.infer_end[fname] - self.infer_start[fname]) * 1000))
 
 
 def main():
     torch.set_grad_enabled(False)
     torch.backends.cudnn.benchmark = True
 
-    test_dir = "../input/deepfake-detection-challenge/test_videos"
-    csv_path = "../input/deepfake-detection-challenge/sample_submission.csv"
+    # test_dir = "../input/deepfake-detection-challenge/test_videos"
+    # csv_path = "../input/deepfake-detection-challenge/sample_submission.csv"
+    test_dir = "./data/train_sample_videos"
+    csv_path = "./dfdc-data.csv"
 
     face_detector = FaceDetector()
-    face_detector.load_checkpoint("../input/dfdc-pretrained-2/RetinaFace-Resnet50-fixed.pth")
+    # face_detector.load_checkpoint("../input/dfdc-pretrained-2/RetinaFace-Resnet50-fixed.pth")
+    face_detector.load_checkpoint("./RetinaFace-Resnet50-fixed.pth")
     loader = DFDCLoader(test_dir, face_detector, T.ToTensor())
 
     model1 = xception(num_classes=2, pretrained=False)
-    ckpt = torch.load("../input/dfdc-pretrained-2/xception-hg-2.pth")
+    # ckpt = torch.load("../input/dfdc-pretrained-2/xception-hg-2.pth")
+    ckpt = torch.load("./xception-hg-2.pth")
     model1.load_state_dict(ckpt["state_dict"])
     model1 = model1.cuda()
     model1.eval()
 
     model2 = WSDAN(num_classes=2, M=8, net="xception", pretrained=False).cuda()
-    ckpt = torch.load("../input/dfdc-pretrained-2/ckpt_x.pth")
+    # ckpt = torch.load("../input/dfdc-pretrained-2/ckpt_x.pth")
+    ckpt = torch.load("./ckpt_x.pth")
     model2.load_state_dict(ckpt["state_dict"])
     model2.eval()
 
     model3 = WSDAN(num_classes=2, M=8, net="efficientnet", pretrained=False).cuda()
-    ckpt = torch.load("../input/dfdc-pretrained-2/ckpt_e.pth")
+    # ckpt = torch.load("../input/dfdc-pretrained-2/ckpt_e.pth")
+    ckpt = torch.load("./ckpt_e.pth")
     model3.load_state_dict(ckpt["state_dict"])
     model3.eval()
 
@@ -142,7 +156,6 @@ def main():
 
     for batch in loader:
         batch = batch.cuda(non_blocking=True)
-
         i1 = F.interpolate(batch, size=299, mode="bilinear")
         i1.sub_(0.5).mul_(2.0)
         o1 = model1(i1).softmax(-1)[:, 1].cpu().numpy()
@@ -158,12 +171,12 @@ def main():
         out = 0.2 * o1 + 0.7 * o2 + 0.1 * o3
         loader.feedback(out)
 
-    with open(csv_path) as fin, open("submission.csv", "w") as fout:
-        fout.write(next(fin))
-        for line in fin:
-            fname = line.split(",", 1)[0]
+    with open("submission.csv", "w") as fout:
+        for fname in loader.file_list:
             pred = loader.score[fname]
-            print("%s,%.6f" % (fname, pred), file=fout)
+            start_time = loader.infer_start[fname]
+            end_time = loader.infer_end[fname]
+            print("%s\t%.6f\t%d\t%d" % (fname.split('.')[0], pred, start_time, end_time), file=fout)
 
 
 if __name__ == "__main__":
